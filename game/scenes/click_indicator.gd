71f0e16eda6b3401ee656203939cf5c6694b8cee
Ludvig Böklin <sudoreboot@protonmail.com>
2016-11-18 11:41:18 +0100

    Refactoring and botched physics calculations

--- game/scenes/click_indicator.gd
+++ game/scenes/click_indicator.gd
@@ -1,10 +1,27 @@
 extends Spatial
 
 
+const RAY_LENGTH = 50
+
 onready var anim = self.get_node("AnimationPlayer")
 
-var ray_from = Vector3()
-var ray_to = Vector3()
+var ray_from = null
+var ray_to = null
+
+
+func set_indicator_pos(input_pos):
+
+	var ray_begin = get_viewport().get_camera().project_position(input_pos)
+	var ray_normal = get_viewport().get_camera().project_ray_normal(input_pos)
+	var ray_vector = ray_normal * RAY_LENGTH
+	var ray_end = ray_begin + ray_vector
+
+	self.ray_from = ray_begin
+	self.ray_to = ray_end
+
+	# I can't seem to find a way to tap into the direct space state
+	# from outside the _fixed_process() - needs further research
+	set_fixed_process(true)
 
 
 func _animation_finished():
@@ -16,14 +33,13 @@ func _fixed_process(delta):
 	var space_state = get_world().get_direct_space_state()
 	# use global coordinates, not local to node
 	var ray_intersect = space_state.intersect_ray( ray_from, ray_to )
-	var pos = ray_intersect.position
 
 	if not ray_intersect.empty():
+		var pos = ray_intersect.position
 		self.set_translation(pos)
 		if get_parent().has_node("Prog"):
 			var prog = get_node("../Prog")
-			prog.jump(pos)
-
+			prog.jump_to(pos)
 
 	set_fixed_process(false)
 
@@ -31,8 +47,6 @@ func _fixed_process(delta):
 func _ready():
 	anim.connect("finished", self, "_animation_finished")
 	anim.play("LeftClick")
-
-	set_fixed_process(true)
 	pass
 
 
--- game/scenes/progs/prog.gd
+++ game/scenes/progs/prog.gd
@@ -3,7 +3,52 @@ extends RigidBody
 
 var destination = null
 var dest_queue = null
-var airborn = false
+
+
+func is_airborn():
+
+	var current_pos = self.get_translation()
+	if current_pos.y <= 0.02:
+		return false
+	else:
+		return true
+
+
+func get_jump_initial_velocity(dest):
+
+	## t is the predetermined time of flight
+#	var t = max(0.5, min(jump_vector_2D.x * 0.7, 1.5))
+	var t = 1
+
+	## angle is the angle at which to launch from the ground
+#	var angle = deg2rad(45)
+
+#	var gravity = self.get_weight()
+
+	## get vector to dest
+	var my_pos = self.get_translation()
+	var jump_vector_3D = dest - my_pos
+
+	## Convert 3D vector into 2D to do some projectile motion math
+	## x is the x and the z axes merged together and y is unchanged
+	var horizontal_distance = Vector2(jump_vector_3D.x, jump_vector_3D.z).length()
+#	var vertical_distance = jump_vector_3D.y
+	## get direction to dest in 2D along the ground
+	var dir = Vector2(jump_vector_3D.x, jump_vector_3D.z).normalized()
+	print(dir.y)
+
+	# Now calculate velocity
+	var horizontal_velocity = horizontal_distance / t
+	var vertical_velocity = horizontal_velocity
+	var initial_velocity = Vector2(horizontal_velocity, vertical_velocity).length()
+
+	## Final velocity translated back into 3D
+	var velocity = Vector3()
+	velocity.x = horizontal_velocity*dir.x
+	velocity.y = vertical_velocity
+	velocity.z = horizontal_velocity*dir.y
+
+	return velocity
 
 
 func attack(pos):
@@ -12,71 +57,42 @@ func attack(pos):
 	pass
 
 
-func jump(dest):
+func jump_to(dest):
 
-	var current_pos = self.get_translation()
-	destination = dest
+	var airborn = self.is_airborn()
 
-	if airborn:
-		dest_queue = dest
-		return
+	if not airborn:
+		self.destination = dest
 	else:
-		dest_queue = null
+		self.dest_queue = dest
 
-	## get length and dir to dest
-	var jump_vector_3D = dest - current_pos
-	## dir is direction along the ground
-	var dir = Vector2(jump_vector_3D.x, jump_vector_3D.z).normalized()
+#	return airborn
 
-	## Convert 3D into 2D
-	## jump_vector_2D.x is distance along ground
-	## jump_vector_2D.y is difference in elevation - most likely 0
-	var jump_vector_2D = Vector2(0, 0)
-	jump_vector_2D.x = Vector2(jump_vector_3D.x, jump_vector_3D.z).length()
-#	jump_vector_2D.y = jump_vector_3D.y
 
-	## t is the predetermined time of flight
-#	var t = max(0.5, min(jump_vector_2D.x * 0.7, 1.5))
-	var t = 1
-	## angle is the angle at which to launch from the ground
-	var angle = deg2rad(45)
-
-	var velocity_2D = Vector2()
-	velocity_2D.x = jump_vector_2D.x
-	velocity_2D.y = velocity_2D.x
-#	velocity_2D *= angle
-	velocity_2D *= sin(2*angle)
-
-	## Final velocity translated into 3D
-	var velocity = Vector3()
-	velocity.x = jump_vector_3D.x / t
-#	velocity.x = velocity_2D.x*dir.x
-	velocity.y = velocity_2D.y
-#	velocity.y = velocity_2D.x * angle / t
-	velocity.z = jump_vector_3D.z / t
-#	velocity.z = velocity_2D.x*dir.y
-
-	## Apply
-	self.set_linear_velocity(velocity)
-	airborn = true
-
+#func _integrate_forces(state):
+#
+#	if self.destination != null:
+#		state.set_linear_velocity(get_jump_initial_velocity(self.destination))
+#		self.destination = null
+#	self.set_use_custom_integrator(false)
 
 
 func _fixed_process(delta):
 
 	var current_pos = self.get_translation()
-	if airborn:
-		if current_pos.y <= 0.02:
-			airborn = false
-	else:
-		if destination != null and (destination - current_pos).length() >= 5:
-			# Set breakpoint here for debug
-			pass
+	if self.is_airborn():
 		return
 
-	if dest_queue != null:
-		jump(dest_queue)
-		dest_queue = null
+	if self.dest_queue != null:
+		self.destination = self.dest_queue
+		self.dest_queue = null
+
+	if self.destination != null:
+		var v = self.get_jump_initial_velocity(self.destination)
+		if v != null:
+			self.set_linear_velocity(v)
+#			self.set_fixed_process(false)
+			self.destination = null
 
 
 func _ready():
